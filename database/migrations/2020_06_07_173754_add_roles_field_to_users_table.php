86507ed6b1dae9e0be51d62ab481d52fa239c373
karapho712 <newhijabmuallif@gmail.com>
2020-06-17 05:37:44 +0800

    update, migration default to ADMIN

--- database/migrations/2020_06_07_173754_add_roles_field_to_users_table.php
+++ database/migrations/2020_06_07_173754_add_roles_field_to_users_table.php
@@ -14,7 +14,7 @@ class AddRolesFieldToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('roles')->default('USER');
+            $table->string('roles')->default('ADMIN');
             //Admin, dan users biasa
         });
     }
